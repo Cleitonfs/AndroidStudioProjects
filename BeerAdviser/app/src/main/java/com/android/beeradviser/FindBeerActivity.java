081c862e995d360fcf283cfbad3867b21890582e
Genilton Cleiton <cleitonfs@yahoo.com>
2017-12-31 12:28:51 -0300

    Refatorando nomes da MainActivity e Layout correspondente

--- BeerAdviser/app/src/main/java/com/android/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/android/beeradviser/FindBeerActivity.java
@@ -3,11 +3,11 @@ package com.android.beeradviser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FindBeerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_find_beer);
     }
 }
