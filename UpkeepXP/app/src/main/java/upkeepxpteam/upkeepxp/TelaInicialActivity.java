efed48bfca4808b223dd52b351eea56d81dade49
Genilton Cleiton <cleitonfs@yahoo.com>
2018-01-17 13:41:18 -0300

    atualizando repositório local

---
+++ UpkeepXP/app/src/main/java/upkeepxpteam/upkeepxp/TelaInicialActivity.java
@@ -0,0 +1,17 @@
+package upkeepxpteam.upkeepxp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    //DrawerActivity drawerActivity = new DrawerActivity();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+
+        //drawerActivity.setVisible(true);
+    }
+}
